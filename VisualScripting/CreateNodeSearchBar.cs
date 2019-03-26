85f051a5605fb68334016842a14a3a267f2f00fa
simonuks221 <simonasriauka@gmail.com>
2019-03-26 16:55:39 +0200

    Create node search bar
    Can now search for available nodes.

--- VisualScripting/CreateNodeSearchBar.cs
+++ VisualScripting/CreateNodeSearchBar.cs
@@ -19,12 +19,15 @@ namespace VisualScripting
 
         VisualClassScriptEditorManager thisVisualScriptManager;
 
+        List<Type> nodesToShow;
+
         //List<Type> nodesToShow = new List<Type>() {typeof(IfNode), typeof(PrintNode), typeof(MakeString)};
 
-        public CreateNodeSearchBar(Point _panelLocation, VisualClassScriptEditorManager _thisVisualScriptManager, List<Type> nodesToShow)
+        public CreateNodeSearchBar(Point _panelLocation, VisualClassScriptEditorManager _thisVisualScriptManager, List<Type> _nodesToShow)
         {
             panelLocation = _panelLocation;
             thisVisualScriptManager = _thisVisualScriptManager;
+            nodesToShow = _nodesToShow;
 
             this.BackColor = Color.DimGray;
             this.Location = _panelLocation;
@@ -34,23 +37,47 @@ namespace VisualScripting
             this.Controls.Add(mainTextBox);
             mainTextBox.Size = new Size(200, 20);
             mainTextBox.Location = new Point(0, 0);
+            mainTextBox.TextChanged += MainTextBoxTextChanged;
 
             for (int i = 0; i < nodesToShow.Count; i++)
             {
-                BaseCreateNodePanelPart newPart = null;
-                if(nodesToShow[i] == typeof(VisualVariable)) //variable
-                {
-                    newPart = new VisualVariableCreatePanelPart(thisVisualScriptManager.visualClass.visualVariables[i]);
-                }
-                else //Not variable
-                {
-                    newPart = new VisualNodeCreatePanelPart(nodesToShow[i]);
-                }
-                this.Controls.Add(newPart);
-                newPart.Location = new Point(0, 20 + i * 15);
-                newPart.panelPressed += PanelPressed;
+                DisplayNodePanelPart(i, this.Controls.Count - 1);
             }
+        }
 
+        private void DisplayNodePanelPart(int _typeIndex, int _listIndex)
+        {
+            BaseCreateNodePanelPart newPart = null;
+            if (nodesToShow[_typeIndex] == typeof(VisualVariable)) //variable
+            {
+                newPart = new VisualVariableCreatePanelPart(thisVisualScriptManager.visualClass.visualVariables[_typeIndex]);
+            }
+            else //Not variable
+            {
+                newPart = new VisualNodeCreatePanelPart(nodesToShow[_typeIndex]);
+            }
+            this.Controls.Add(newPart);
+            newPart.Location = new Point(0, 20 + _listIndex * 15);
+            newPart.panelPressed += PanelPressed;
+        }
+
+        private void MainTextBoxTextChanged(object sender, EventArgs e)
+        {
+            int amountOfChildren = this.Controls.Count - 1; //Clear children, dont deletete the text box
+            for (int i = 0; i < amountOfChildren; i++)
+            {
+                this.Controls[1].Dispose();
+            }
+            
+            for(int i = 0; i < nodesToShow.Count; i++)
+            {
+                string typeName = nodesToShow[i].GetField("nodeName").GetValue(null).ToString().ToLower();
+
+                if (typeName.Contains(mainTextBox.Text.ToLower()))
+                {
+                    DisplayNodePanelPart(i, this.Controls.Count - 1);
+                }
+            }
         }
 
         private void PanelPressed(BaseCreateNodePanelPart _panel)
